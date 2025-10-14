32216bc2485aaafbec1dc7ac325cd915b9ffe2b6
Arman Mojaver <arman_mojaver@hotmail.com>
2025-10-14 14:18:10 +0200

    tests: add "session" and "_setup_test_database" fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,13 @@
 import os
+from collections.abc import Generator
 
 import pytest
+from alembic import command
+from alembic.config import Config
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy_utils import create_database, database_exists, drop_database
 
 from src.main import app
 
@@ -17,3 +23,37 @@ if not project_config.is_testing():
 def client():
     with TestClient(app) as client:
         yield client
+
+
+@pytest.fixture(scope="session")
+def debug(request) -> bool:
+    return request.config.getoption("--d")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def _setup_test_database(*, debug: bool) -> Generator[None]:
+    """Create the test database, runs migrations, and tear it down afterward."""
+    if database_exists(project_config.SQLALCHEMY_DATABASE_URI):
+        drop_database(project_config.SQLALCHEMY_DATABASE_URI)
+    create_database(project_config.SQLALCHEMY_DATABASE_URI)
+
+    alembic_cfg = Config("alembic.ini")
+    command.upgrade(alembic_cfg, "head")
+
+    yield
+
+    if not debug:
+        drop_database(project_config.SQLALCHEMY_DATABASE_URI)
+
+
+@pytest.fixture
+def session() -> Session:
+    """Provide a SQLAlchemy session for tests."""
+    engine = create_engine(project_config.SQLALCHEMY_DATABASE_URI)
+    Session = sessionmaker(bind=engine, expire_on_commit=False)  # noqa: N806
+    session = Session()
+
+    yield session
+
+    session.close()
+    engine.dispose()
