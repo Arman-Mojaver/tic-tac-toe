a136be2fa66a3dd0e85648d797df1e3ef95ee3fc
Arman Mojaver <arman_mojaver@hotmail.com>
2025-10-15 09:55:00 +0200

    tests: add order to create_move() function calls

--- tests/views/status/no_winner_test.py
+++ tests/views/status/no_winner_test.py
@@ -1,6 +1,6 @@
 def test_in_progress_game_with_one_move_returns_status(client, create_match, create_move):
     match = create_match()
-    create_move(match=match, user=match.user_x, x=1, y=1)
+    create_move(match=match, user=match.user_x, x=1, y=1, order=1)
     response = client.get("/status", params={"match_id": match.id})
     assert response.status_code == 200
     assert response.json() == {
@@ -22,8 +22,8 @@ def test_in_progress_game_with_two_moves_returns_status(
     create_move,
 ):
     match = create_match()
-    create_move(match=match, user=match.user_x, x=1, y=1)
-    create_move(match=match, user=match.user_o, x=0, y=0)
+    create_move(match=match, user=match.user_x, x=1, y=1, order=1)
+    create_move(match=match, user=match.user_o, x=0, y=0, order=2)
     response = client.get("/status", params={"match_id": match.id})
     assert response.status_code == 200
     assert response.json() == {
@@ -45,9 +45,9 @@ def test_in_progress_game_with_three_moves_returns_status(
     create_move,
 ):
     match = create_match()
-    create_move(match=match, user=match.user_x, x=1, y=1)
-    create_move(match=match, user=match.user_o, x=0, y=0)
-    create_move(match=match, user=match.user_x, x=1, y=2)
+    create_move(match=match, user=match.user_x, x=1, y=1, order=1)
+    create_move(match=match, user=match.user_o, x=0, y=0, order=2)
+    create_move(match=match, user=match.user_x, x=1, y=2, order=3)
     response = client.get("/status", params={"match_id": match.id})
     assert response.status_code == 200
     assert response.json() == {
@@ -69,10 +69,10 @@ def test_in_progress_game_with_four_moves_returns_status(
     create_move,
 ):
     match = create_match()
-    create_move(match=match, user=match.user_x, x=1, y=1)
-    create_move(match=match, user=match.user_o, x=0, y=0)
-    create_move(match=match, user=match.user_x, x=1, y=2)
-    create_move(match=match, user=match.user_o, x=1, y=0)
+    create_move(match=match, user=match.user_x, x=1, y=1, order=1)
+    create_move(match=match, user=match.user_o, x=0, y=0, order=2)
+    create_move(match=match, user=match.user_x, x=1, y=2, order=3)
+    create_move(match=match, user=match.user_o, x=1, y=0, order=4)
     response = client.get("/status", params={"match_id": match.id})
     assert response.status_code == 200
     assert response.json() == {
@@ -94,11 +94,11 @@ def test_in_progress_game_with_five_moves_returns_status(
     create_move,
 ):
     match = create_match()
-    create_move(match=match, user=match.user_x, x=1, y=1)
-    create_move(match=match, user=match.user_o, x=0, y=0)
-    create_move(match=match, user=match.user_x, x=1, y=2)
-    create_move(match=match, user=match.user_o, x=1, y=0)
-    create_move(match=match, user=match.user_x, x=2, y=0)
+    create_move(match=match, user=match.user_x, x=1, y=1, order=1)
+    create_move(match=match, user=match.user_o, x=0, y=0, order=2)
+    create_move(match=match, user=match.user_x, x=1, y=2, order=3)
+    create_move(match=match, user=match.user_o, x=1, y=0, order=4)
+    create_move(match=match, user=match.user_x, x=2, y=0, order=5)
     response = client.get("/status", params={"match_id": match.id})
     assert response.status_code == 200
     assert response.json() == {
@@ -120,12 +120,12 @@ def test_in_progress_game_with_six_moves_returns_status(
     create_move,
 ):
     match = create_match()
-    create_move(match=match, user=match.user_x, x=1, y=1)
-    create_move(match=match, user=match.user_o, x=0, y=0)
-    create_move(match=match, user=match.user_x, x=1, y=2)
-    create_move(match=match, user=match.user_o, x=1, y=0)
-    create_move(match=match, user=match.user_x, x=2, y=0)
-    create_move(match=match, user=match.user_o, x=0, y=2)
+    create_move(match=match, user=match.user_x, x=1, y=1, order=1)
+    create_move(match=match, user=match.user_o, x=0, y=0, order=2)
+    create_move(match=match, user=match.user_x, x=1, y=2, order=3)
+    create_move(match=match, user=match.user_o, x=1, y=0, order=4)
+    create_move(match=match, user=match.user_x, x=2, y=0, order=5)
+    create_move(match=match, user=match.user_o, x=0, y=2, order=6)
     response = client.get("/status", params={"match_id": match.id})
     assert response.status_code == 200
     assert response.json() == {
@@ -147,13 +147,13 @@ def test_in_progress_game_with_seven_moves_returns_status(
     create_move,
 ):
     match = create_match()
-    create_move(match=match, user=match.user_x, x=1, y=1)
-    create_move(match=match, user=match.user_o, x=0, y=0)
-    create_move(match=match, user=match.user_x, x=1, y=2)
-    create_move(match=match, user=match.user_o, x=1, y=0)
-    create_move(match=match, user=match.user_x, x=2, y=0)
-    create_move(match=match, user=match.user_o, x=0, y=2)
-    create_move(match=match, user=match.user_x, x=0, y=1)
+    create_move(match=match, user=match.user_x, x=1, y=1, order=1)
+    create_move(match=match, user=match.user_o, x=0, y=0, order=2)
+    create_move(match=match, user=match.user_x, x=1, y=2, order=3)
+    create_move(match=match, user=match.user_o, x=1, y=0, order=4)
+    create_move(match=match, user=match.user_x, x=2, y=0, order=5)
+    create_move(match=match, user=match.user_o, x=0, y=2, order=6)
+    create_move(match=match, user=match.user_x, x=0, y=1, order=7)
     response = client.get("/status", params={"match_id": match.id})
     assert response.status_code == 200
     assert response.json() == {
@@ -175,14 +175,14 @@ def test_in_progress_game_with_eight_moves_returns_status(
     create_move,
 ):
     match = create_match()
-    create_move(match=match, user=match.user_x, x=1, y=1)
-    create_move(match=match, user=match.user_o, x=0, y=0)
-    create_move(match=match, user=match.user_x, x=1, y=2)
-    create_move(match=match, user=match.user_o, x=1, y=0)
-    create_move(match=match, user=match.user_x, x=2, y=0)
-    create_move(match=match, user=match.user_o, x=0, y=2)
-    create_move(match=match, user=match.user_x, x=0, y=1)
-    create_move(match=match, user=match.user_o, x=2, y=2)
+    create_move(match=match, user=match.user_x, x=1, y=1, order=1)
+    create_move(match=match, user=match.user_o, x=0, y=0, order=2)
+    create_move(match=match, user=match.user_x, x=1, y=2, order=3)
+    create_move(match=match, user=match.user_o, x=1, y=0, order=4)
+    create_move(match=match, user=match.user_x, x=2, y=0, order=5)
+    create_move(match=match, user=match.user_o, x=0, y=2, order=6)
+    create_move(match=match, user=match.user_x, x=0, y=1, order=7)
+    create_move(match=match, user=match.user_o, x=2, y=2, order=8)
     response = client.get("/status", params={"match_id": match.id})
     assert response.status_code == 200
     assert response.json() == {
@@ -204,15 +204,15 @@ def test_in_progress_game_with_nine_moves_returns_status(
     create_move,
 ):
     match = create_match()
-    create_move(match=match, user=match.user_x, x=1, y=1)
-    create_move(match=match, user=match.user_o, x=0, y=0)
-    create_move(match=match, user=match.user_x, x=1, y=2)
-    create_move(match=match, user=match.user_o, x=1, y=0)
-    create_move(match=match, user=match.user_x, x=2, y=0)
-    create_move(match=match, user=match.user_o, x=0, y=2)
-    create_move(match=match, user=match.user_x, x=0, y=1)
-    create_move(match=match, user=match.user_o, x=2, y=2)
-    create_move(match=match, user=match.user_x, x=2, y=1)
+    create_move(match=match, user=match.user_x, x=1, y=1, order=1)
+    create_move(match=match, user=match.user_o, x=0, y=0, order=2)
+    create_move(match=match, user=match.user_x, x=1, y=2, order=3)
+    create_move(match=match, user=match.user_o, x=1, y=0, order=4)
+    create_move(match=match, user=match.user_x, x=2, y=0, order=5)
+    create_move(match=match, user=match.user_o, x=0, y=2, order=6)
+    create_move(match=match, user=match.user_x, x=0, y=1, order=7)
+    create_move(match=match, user=match.user_o, x=2, y=2, order=8)
+    create_move(match=match, user=match.user_x, x=2, y=1, order=9)
 
     response = client.get("/status", params={"match_id": match.id})
     assert response.status_code == 200
--- tests/views/status/winner_test.py
+++ tests/views/status/winner_test.py
@@ -3,11 +3,11 @@ def test_x_won(client, create_match, create_move, create_user):
     user_o = create_user()
 
     match = create_match(user_x=user_x, user_o=user_o, winner=user_x)
-    create_move(match=match, user=match.user_x, x=0, y=0)
-    create_move(match=match, user=match.user_o, x=1, y=0)
-    create_move(match=match, user=match.user_x, x=0, y=1)
-    create_move(match=match, user=match.user_o, x=1, y=1)
-    create_move(match=match, user=match.user_x, x=0, y=2)
+    create_move(match=match, user=match.user_x, x=0, y=0, order=1)
+    create_move(match=match, user=match.user_o, x=1, y=0, order=2)
+    create_move(match=match, user=match.user_x, x=0, y=1, order=3)
+    create_move(match=match, user=match.user_o, x=1, y=1, order=4)
+    create_move(match=match, user=match.user_x, x=0, y=2, order=5)
 
     response = client.get("/status", params={"match_id": match.id})
     assert response.status_code == 200
@@ -29,12 +29,12 @@ def test_o_won(client, create_match, create_move, create_user):
     user_o = create_user()
 
     match = create_match(user_x=user_x, user_o=user_o, winner=user_o)
-    create_move(match=match, user=match.user_x, x=0, y=0)
-    create_move(match=match, user=match.user_o, x=1, y=0)
-    create_move(match=match, user=match.user_x, x=0, y=1)
-    create_move(match=match, user=match.user_o, x=1, y=1)
-    create_move(match=match, user=match.user_x, x=2, y=2)
-    create_move(match=match, user=match.user_o, x=1, y=2)
+    create_move(match=match, user=match.user_x, x=0, y=0, order=1)
+    create_move(match=match, user=match.user_o, x=1, y=0, order=2)
+    create_move(match=match, user=match.user_x, x=0, y=1, order=3)
+    create_move(match=match, user=match.user_o, x=1, y=1, order=4)
+    create_move(match=match, user=match.user_x, x=2, y=2, order=5)
+    create_move(match=match, user=match.user_o, x=1, y=2, order=6)
 
     response = client.get("/status", params={"match_id": match.id})
     assert response.status_code == 200
